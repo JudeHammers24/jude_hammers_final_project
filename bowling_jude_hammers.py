7328e385543a6f2c4caba83858d11f238905c3e0
JudeHammers24 <142941815+JudeHammers24@users.noreply.github.com>
2023-12-01 08:51:29 -0800

    Add files via upload
    First Push

---
+++ bowling_jude_hammers.py
@@ -0,0 +1,14 @@
+# This file was created by Jude Hammers
+
+import pygame
+
+pygame.init()
+
+SCREEN_WIDTH = 200
+SCREEN_HEIGHT = 800
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+run = True
+while run == True:
+    run
